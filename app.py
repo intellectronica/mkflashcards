527925657000ff83f1895522543c054b69a18c86
Eleanor Berger <elle.fnord@gmail.com>
2024-08-30 13:35:08 +0000

    Remove fh. prefix

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-import fasthtml.common as fh
+from fasthtml.common import *
 from markdown import markdown
 
 from mkflashcards import *
@@ -9,8 +9,8 @@ def app_js():
     with open('app.js') as f:
         return f.read()
 
-app, rt = fh.fast_app(
-    hdrs=[fh.Script(app_js())],
+app, rt = fast_app(
+    hdrs=[Script(app_js())],
 )
 
 @app.post('/-/fetch-text')
@@ -23,54 +23,54 @@ async def do_fetch_text(openai_api_key: str, model: str, num_flashcards: int, ta
 
 @app.get("/")
 def home():
-    return fh.Form(
-        fh.Container(
-            fh.Card(fh.NotStr(markdown(ABOUT))),
-            fh.Grid(
-                fh.Group(
-                    fh.B('OPENAI_API_KEY'),
-                    fh.Input(name='openai_api_key', type='password', value=os.getenv('OPENAI_API_KEY', ''), id='openai_api_key'),
+    return Form(
+        Container(
+            Card(NotStr(markdown(ABOUT))),
+            Grid(
+                Group(
+                    B('OPENAI_API_KEY'),
+                    Input(name='openai_api_key', type='password', value=os.getenv('OPENAI_API_KEY', ''), id='openai_api_key'),
                 ),
-                fh.Group(
-                    fh.B('Model'),
-                    fh.Select(
-                        fh.Option('gpt-4o-mini', selected=True),
-                        fh.Option('gpt-4o-2024-08-06'),
+                Group(
+                    B('Model'),
+                    Select(
+                        Option('gpt-4o-mini', selected=True),
+                        Option('gpt-4o-2024-08-06'),
                         name='model', id='model',
                     ),
                 ),
             ),
-            fh.Grid(
-                fh.Group(
-                    fh.B('JINA_API_KEY'),
-                    fh.Input(name='jina_api_key', type='password', value=os.getenv('JINA_API_KEY', ''), id='jina_api_key'),
+            Grid(
+                Group(
+                    B('JINA_API_KEY'),
+                    Input(name='jina_api_key', type='password', value=os.getenv('JINA_API_KEY', ''), id='jina_api_key'),
                 ),
-                fh.Group(
-                    fh.B('URL'),
-                    fh.Input(name='url', type='text', id='url'),
+                Group(
+                    B('URL'),
+                    Input(name='url', type='text', id='url'),
                 ),
-                fh.Button('Fetch Text', hx_post='/-/fetch-text', hx_target='#text', hx_swap='innerHTML'),
+                Button('Fetch Text', hx_post='/-/fetch-text', hx_target='#text', hx_swap='innerHTML'),
             ),
-            fh.Group(
-                fh.B('Text'),
-                fh.Textarea(name='text', rows=7, id='text'),
+            Group(
+                B('Text'),
+                Textarea(name='text', rows=7, id='text'),
             ),
-            fh.Grid(
-                fh.Group(
-                    fh.B('Number of flashcards to generate'),
-                    fh.Input(name='num_flashcards', type='number', value=23, id='num_flashcards'),
+            Grid(
+                Group(
+                    B('Number of flashcards to generate'),
+                    Input(name='num_flashcards', type='number', value=23, id='num_flashcards'),
                 ),
-                fh.Group(
-                    fh.B('Tags'),
-                    fh.Input(name='tags', type='text', id='tags'),
+                Group(
+                    B('Tags'),
+                    Input(name='tags', type='text', id='tags'),
                 ),
-                fh.Button('Generate Flashcards', hx_post='/-/generate-flashcards', hx_target='#flashcards', hx_swap='innerHTML'),
+                Button('Generate Flashcards', hx_post='/-/generate-flashcards', hx_target='#flashcards', hx_swap='innerHTML'),
             ),
-            fh.Group(
-                fh.B('Flashcards'),
-                fh.Textarea(rows=23, id='flashcards'),
+            Group(
+                B('Flashcards'),
+                Textarea(rows=23, id='flashcards'),
             ),
         ),
     )
 
-fh.serve()
+serve()
